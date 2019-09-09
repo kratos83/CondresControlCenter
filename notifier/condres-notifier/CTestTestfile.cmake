362e78862047db3aaf7e162ffae4187f523a0d4a
kratos83 <angelo.scarna@codelinsoft.it>
2019-09-09 21:19:55 +0200

    CondresControlCenter 0.3.3

--- notifier/condres-notifier/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/angelo/Progetti/progetti_completi/control_center/CondresControlCenter/notifier/condres-notifier
-# Build directory: /home/angelo/Progetti/progetti_completi/control_center/CondresControlCenter/notifier/condres-notifier
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
